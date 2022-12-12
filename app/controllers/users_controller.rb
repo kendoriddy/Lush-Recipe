72395cd472f107fc5cc93f3cb2d371aa81ca0564
Kehinde Onifade <onifkay@gmail.com>
2022-12-12 15:59:19 +0100

    set up signup and signin

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        
+    end
+
+    def show
+        
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :user, only: [:index, :show, :create, :destroy] do
+    resources :food, only: [:index, :show, :create, :destroy]
+    resources :recipe, only: [:index, :show, :create, :destroy]
+  end
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
