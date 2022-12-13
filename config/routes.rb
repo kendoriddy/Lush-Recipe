e34b5401650f1bb6ada5fe4fbc5e48afc5c500c0
Anas <anas.chaabane98@gmail.com>
2022-12-13 16:36:13 +0100

    implement Recipe#index-Recipe#shpw-Recipe#new-Recipe#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
 
-  resources :users, only: [:index, :show, :create, :destroy] do
-    resources :foods, only: [:index, :show, :create, :destroy]
-    resources :recipes, only: [:index, :show, :create, :destroy]
+  resources :foods
+  resources :recipes
+  
 
-  end
 
   # Defines the root path route ("/")
 
