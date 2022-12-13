3e6747efc1a5cafe63bb74648f346ac4fbf36296
Kehinde Onifade <onifkay@gmail.com>
2022-12-13 12:50:58 +0100

    add food delete and update controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -16,10 +16,26 @@ class FoodsController < ApplicationController
         if food.save
             redirect_to foods_path, notice: 'New Food successfully added.'
         else
-            flash[:alert] = 'New Food adding Failed. Please try again.'
+            flash[:alert] = 'Food adding Failed. Please try again.'
         end
     end
 
     def destroy
+        food = Food.find(params[:id])
+
+        unless food.user = current_user
+            return flash[:alert] = "You do not have permission to delete this food as it belongs to another user"
+        end
+
+        if food.destroy
+            flash[:notice] = 'Food deleted successfully.'
+          else
+            flash[:alert] = 'Food deleting Failed. Please try again.'
+          end
+          redirect_back(fallback_location: root_path)
+    end
+
+    def food_params
+        params.require(:food).permit(:name, :quantity, :measurement_unit, :price)
     end
 end
