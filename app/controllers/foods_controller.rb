64eef9e71e2cd4a9cd5e699b0b55d596cc939ace
Kehinde Onifade <onifkay@gmail.com>
2022-12-13 12:45:39 +0100

    add food create

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,7 +6,18 @@ class FoodsController < ApplicationController
     def show
     end
 
+    def new
+        @food = Food.new
+    end
+
     def create
+        food = current_user.foods.new(food_params)
+
+        if food.save
+            redirect_to foods_path, notice: 'New Food successfully added.'
+        else
+            flash[:alert] = 'New Food adding Failed. Please try again.'
+        end
     end
 
     def destroy
