d410711483ad6e1b100e422a9cf3f6477bd7f7ba
Kehinde Onifade <onifkay@gmail.com>
2022-12-14 20:56:34 +0100

    update schema

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
 
-<<<<<<< HEAD
-    resources :foods
-    resources :recipes
-=======
   resources :foods
   resources :recipes
-  
 
->>>>>>> 1c55234d82a45875783b356aa106db834fdce826
+
 
   # Defines the root path route ("/")
 
