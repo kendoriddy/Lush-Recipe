b30d85c03801928ee4500a733ee24c3909a87809
Kehinde Onifade <onifkay@gmail.com>
2022-12-14 10:46:48 +0100

    Revert "Recipe implementation"

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,5 @@
 class RecipesController < ApplicationController
   skip_before_action :authenticate_user!
-  before_action :set_user
-  before_action :set_recipe, only: [:show, :destroy]
-   
   def index
     @user = current_user
     @recipes = @user.recipes
@@ -10,41 +7,14 @@ class RecipesController < ApplicationController
 
 
   def show  
+
   end 
 
 
   def destroy
-    @recipe.destroy
-    redirect_to recipes_path
-  end 
 
-  def new
-    @recipe = Recipe.new
-  end 
-
-  def create 
-    @recipe = Recipe.new(recipe_params)
-    @recipe.user = @user
-    if @recipe.save 
-      redirect_to recipes_path, notice: "recipe created successfully"
-    else
-      render :new, notice: "notsaved"
-    end 
-  end 
-
-  private
-
-  def set_user
-    @user = current_user
-  end 
-
-  def set_recipe 
-    @recipe = set_user.recipes.find(params[:id])
   end 
     
 
-  def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
-  end
 
 end
