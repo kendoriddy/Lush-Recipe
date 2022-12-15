c36ddac7bb56149867e148189c15cd6f835c6b5e
Anas <anas.chaabane98@gmail.com>
2022-12-15 15:18:36 +0100

    resolve bootstrap related problem

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,4 +10,4 @@ import { application } from "./application";
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+// lazyLoadControllersFrom("controllers", application) 
