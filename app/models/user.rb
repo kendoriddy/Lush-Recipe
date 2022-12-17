9b0651321475f75f5d47393abf46144a428f12b0
Anas <anas.chaabane98@gmail.com>
2022-12-17 09:54:45 +0100

    adname filed to devise regestration form and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
        has_many :recipes
        has_many :foods
 
+       validates_associated :foods
+       validates_associated :recipes
+       validates :name, :login, :email, presence: true
+       validates :email, uniqueness: true
+       validates :name, length: { minimum: 2 }
+       validates :password, length: { minimum: 6 }
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
