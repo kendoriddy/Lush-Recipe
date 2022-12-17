0a8c18ab1abee04449715cc2db50336c6d787caa
Kehinde Onifade <onifkay@gmail.com>
2022-12-17 11:34:08 +0100

    testing first commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :cooking_time, presence: true, length: { maximum: 20 }
+  validates :preparation_time, presence: true, length: { maximum: 20 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-
+         validates :name, presence: true, length: { maximum: 50 }
 end
