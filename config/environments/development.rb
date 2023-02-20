776c31914d2408a87f32882c7d107b5516697d76
Kehinde Onifade <onifkay@gmail.com>
2023-02-20 20:39:10 +0100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.delivery_method = :letter_opener
-config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
 end
